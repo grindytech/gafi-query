309e218b62159554a3b19bed8ba04f195811ffa9
Hoang Trung <lhtrung307@gmail.com>
2022-05-14 17:13:53 +0700

    Add BlockEntity for temporary handle block and call

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,14 +1,14 @@
 import { SubstrateExtrinsic, SubstrateEvent, SubstrateBlock } from "@subql/types";
-import { Transfer } from "../types";
+import { Transfer, BlockEntity } from "../types";
 import { Balance } from "@polkadot/types/interfaces";
 
-// export async function handleBlock(block: SubstrateBlock): Promise<void> {
-//   //Create a new starterEntity with ID using block hash
-//   let record = new StarterEntity(block.block.header.hash.toString());
-//   //Record block number
-//   record.field1 = block.block.header.number.toNumber();
-//   await record.save();
-// }
+export async function handleBlock(block: SubstrateBlock): Promise<void> {
+  //Create a new starterEntity with ID using block hash
+  let record = new BlockEntity(block.block.header.hash.toString());
+  //Record block number
+  record.field1 = block.block.header.number.toNumber();
+  await record.save();
+}
 
 export async function handleEvent(event: SubstrateEvent): Promise<void> {
   const [from, to, amount, ...rest] = event.event.data;
@@ -22,11 +22,11 @@ export async function handleEvent(event: SubstrateEvent): Promise<void> {
   await transfer.save();
 }
 
-// export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-//   const record = await StarterEntity.get(extrinsic.block.block.header.hash.toString());
-//   //Date type timestamp
-//   record.field4 = extrinsic.block.timestamp;
-//   //Boolean tyep
-//   record.field5 = true;
-//   await record.save();
-// }
+export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
+  const record = await BlockEntity.get(extrinsic.block.block.header.hash.toString());
+  //Date type timestamp
+  record.field4 = extrinsic.block.timestamp;
+  //Boolean tyep
+  record.field5 = true;
+  await record.save();
+}
