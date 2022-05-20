33e9c2bf4d567aee5d61aff349b29739002cea45
Hoang Trung <lhtrung307@gmail.com>
2022-05-20 10:23:37 +0700

    Refactor mapping handler. Change some docker config. Add local env for project

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -3,9 +3,8 @@ import { Transfer, BlockEntity, SponsoredPool } from "../types";
 import { Balance } from "@polkadot/types/interfaces";
 
 export async function handleBlock(block: SubstrateBlock): Promise<void> {
-  //Create a new starterEntity with ID using block hash
   let record = new BlockEntity(block.block.header.hash.toString());
-  //Record block number
+
   record.field1 = block.block.header.number.toNumber();
   await record.save();
 }
@@ -24,24 +23,27 @@ export async function handleEvent(event: SubstrateEvent): Promise<void> {
 
 export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
   const record = await BlockEntity.get(extrinsic.block.block.header.hash.toString());
-  //Date type timestamp
+
   record.field4 = extrinsic.block.timestamp;
-  //Boolean tyep
   record.field5 = true;
   await record.save();
 }
-export async function handleSponsoredPoolCreate(event: SubstrateEvent): Promise<void> {
-  const createdPool = new SponsoredPool(`${event.block.block.header.number.toNumber()}-${event.idx}`);
-  //Date type timestamp
-  const { extrinsic } = event;
-  if (extrinsic) {
-    createdPool.poolOwner = extrinsic.extrinsic.signer.toString();
-    createdPool.amount = extrinsic.extrinsic.args[1] as unknown as bigint;
-    createdPool.discount = extrinsic.extrinsic.args[2] as unknown as bigint;
-    createdPool.txLimit = extrinsic.extrinsic.args[3] as unknown as number;
 
-    //Boolean tyep
-    createdPool.createdAt = extrinsic.block.timestamp;
-    await createdPool.save();
+// index create sponsored pool to show list of them in dashboard. Temporary get data from args.
+export async function handleCreateSponsoredPool(event: SubstrateEvent): Promise<void> {
+  const createdPool = new SponsoredPool(`${event.block.block.header.number.toNumber()}-${event.idx}`);
+  logger.info(`created pool ${event.block.hash.toString()}`);
+  const { extrinsic: eventExtrinsic } = event;
+
+  if (eventExtrinsic) {
+    const { extrinsic, block, success } = eventExtrinsic;
+    if (success) {
+      createdPool.createdAt = block.timestamp;
+      createdPool.poolOwner = extrinsic.signer.toString();
+      createdPool.amount = extrinsic.args[1] as unknown as bigint;
+      createdPool.discount = extrinsic.args[2] as unknown as number;
+      createdPool.txLimit = extrinsic.args[3] as unknown as number;
+      await createdPool.save();
+    }
   }
 }
